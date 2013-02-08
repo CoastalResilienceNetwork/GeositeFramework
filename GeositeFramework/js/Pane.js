dd51bbd359ffbb932135ab15334a0964743ff012
Rick Mohr <rmohr@azavea.com>
2013-02-08 14:45:42 -0500

    Simplify Pane.js to use 2 anonymous scopes instead of 3
    Rather than using one outer scope with inner scopes for the model and the view, just use outer scopes for both the model and view. That's better anyway since they shouldn't be sharing scope members.
    
    (The diff is not helpful here -- only the scopes changed, not the code.)

--- GeositeFramework/js/Pane.js
+++ GeositeFramework/js/Pane.js
@@ -5,112 +5,110 @@
 
 (function (N) {
     'use strict';
-    (function () {
-        var model = null;
+    var model = null;
 
-        function initializePane(paneModel) {
-            model = paneModel;
-            createPlugins();
-        }
+    function initializePane(paneModel) {
+        model = paneModel;
+        createPlugins();
+    }
 
-        function createPlugins() {
-            model.set('plugins', []);
+    function createPlugins() {
+        model.set('plugins', []);
 
-            _.each(N.plugins, function (pluginClass) {
-                var plugin = new pluginClass();
-                model.get('plugins').push(plugin);
-            });
-        }
-
-        // initPlugins() is separate from createPlugins() because:
-        //     - We need to create plugin objects before rendering (so we can render their toolbar names).
-        //     - We need to pass a map object to the plugin constructors, but that isn't available until after rendering. 
-
-        function initPlugins(esriMap) {
-            var mapWrapper = getMapWrapper(esriMap);
-            _.each(model.get('plugins'), function (plugin) {
-                plugin.constructor({
-                    map: mapWrapper
-                });
-            });
-        }
-
-        function getMapWrapper(esriMap) {
-            var wrapper = _.extend({}, esriMap);
-            return wrapper;
-        }
-
-        N.models = N.models || {};
-        N.models.Pane = Backbone.Model.extend({
-            initialize: function () {
-                initializePane(this);
-            },
-            initPlugins: initPlugins
+        _.each(N.plugins, function (pluginClass) {
+            var plugin = new pluginClass();
+            model.get('plugins').push(plugin);
         });
+    }
 
-    }());
+    // initPlugins() is separate from createPlugins() because:
+    //     - We need to create plugin objects before rendering (so we can render their toolbar names).
+    //     - We need to pass a map object to the plugin constructors, but that isn't available until after rendering. 
 
-    (function () {
-        var view = null;
-        var model = null;
-
-        function renderPane(paneView) {
-            view = paneView;
-            model = view.model;
-            renderSelf();
-            renderPlugins();
-            renderSidebarLinks();
-            return view;
-        }
-
-        function renderSelf() {
-            var paneTemplate = N.app.templates['template-pane'],
-                html = paneTemplate();
-            view.$el.append(html);
-        }
-
-        function renderPlugins() {
-            var regionData = model.get('regionData'),
-                plugins = model.get('plugins'),
-                toolTemplate = N.app.templates['template-sidebar-plugin'],
-                $tools = view.$('.plugins');
-            _.each(plugins, function (plugin) {
-                var html = toolTemplate({ toolbarName: plugin.toolbarName });
-                $tools.append(html);
+    function initPlugins(esriMap) {
+        var mapWrapper = getMapWrapper(esriMap);
+        _.each(model.get('plugins'), function (plugin) {
+            plugin.constructor({
+                map: mapWrapper
             });
-        }
-
-        function renderSidebarLinks() {
-            var regionData = model.get('regionData'),
-                linkTemplate = N.app.templates['template-sidebar-link'],
-                $links = view.$('.sidebar-links');
-            _.each(regionData.sidebarLinks, function (link) {
-                var html = linkTemplate({ link: link });
-                $links.append(html);
-            });
-        }
-
-        function createMap() {
-            var paneNumber = model.get('paneNumber'),
-                $map = view.$('.map'),
-                domId = "map" + paneNumber;
-            $map.attr("id", domId);
-            var esriMap = new esri.Map(domId, {
-                // center: [-56.049, 38.485],
-                zoom: 4,
-                basemap: "streets"
-            });
-            return esriMap;
-        }
-        
-        N.views = N.views || {};
-        N.views.Pane = Backbone.View.extend({
-            render: function () {
-                renderPane(this);
-            },
-            createMap: createMap
         });
+    }
 
-    }())
+    function getMapWrapper(esriMap) {
+        var wrapper = _.extend({}, esriMap);
+        return wrapper;
+    }
+
+    N.models = N.models || {};
+    N.models.Pane = Backbone.Model.extend({
+        initialize: function () {
+            initializePane(this);
+        },
+        initPlugins: initPlugins
+    });
+
+}(Geosite));
+
+(function (N) {
+    'use strict';
+    var view = null;
+    var model = null;
+
+    function renderPane(paneView) {
+        view = paneView;
+        model = view.model;
+        renderSelf();
+        renderPlugins();
+        renderSidebarLinks();
+        return view;
+    }
+
+    function renderSelf() {
+        var paneTemplate = N.app.templates['template-pane'],
+            html = paneTemplate();
+        view.$el.append(html);
+    }
+
+    function renderPlugins() {
+        var regionData = model.get('regionData'),
+            plugins = model.get('plugins'),
+            toolTemplate = N.app.templates['template-sidebar-plugin'],
+            $tools = view.$('.plugins');
+        _.each(plugins, function (plugin) {
+            var html = toolTemplate({ toolbarName: plugin.toolbarName });
+            $tools.append(html);
+        });
+    }
+
+    function renderSidebarLinks() {
+        var regionData = model.get('regionData'),
+            linkTemplate = N.app.templates['template-sidebar-link'],
+            $links = view.$('.sidebar-links');
+        _.each(regionData.sidebarLinks, function (link) {
+            var html = linkTemplate({ link: link });
+            $links.append(html);
+        });
+    }
+
+    function createMap() {
+        var paneNumber = model.get('paneNumber'),
+            $map = view.$('.map'),
+            domId = "map" + paneNumber;
+        $map.attr("id", domId);
+        var esriMap = new esri.Map(domId, {
+            // center: [-56.049, 38.485],
+            zoom: 4,
+            basemap: "streets"
+        });
+        return esriMap;
+    }
+
+    N.views = N.views || {};
+    N.views.Pane = Backbone.View.extend({
+        render: function () {
+            renderPane(this);
+        },
+        createMap: createMap
+    });
 
 }(Geosite));
