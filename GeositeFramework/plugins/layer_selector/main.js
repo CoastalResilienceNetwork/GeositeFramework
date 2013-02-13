bad11143c03690e79d9f53674862f59e41949b17
Rick Mohr <rmohr@azavea.com>
2013-02-13 12:30:59 -0500

    Create agsLoader.js and ui.js modules from main.js

--- GeositeFramework/plugins/layer_selector/main.js
+++ GeositeFramework/plugins/layer_selector/main.js
@@ -1,186 +1,25 @@
 ﻿define(
-    ["dojo/_base/declare"],
-    function (declare) {
-
-        var baseUrl = 'http://dev.gulfmex.coastalresilience.org/arcgis/rest/services';
-        var rootNode = { expanded: true, children: [] };
-
-        // Load hierarchy of folders, services, and layers from an ArcGIS Server via its REST API.
-        // The catalog root contains folders and/or services.
-        // Each folder contains additional services.
-        // Each "MapServer" service exposes a number of layers.
-        // A layer entry may actually be a group, containing other layers in the same collection.
-
-        // Use the catalog data to build a node tree for Ext.data.TreeStore and Ext.tree.Panel
-
-        function loadCatalog() {
-            // Load root catalog entries
-            loadFolder("", function (entries) {
-                console.log("Catalog has " + entries.folders.length + " folders");
-                // Root of catalog has loaded -- load child folders and services
-                addParentNodeToServiceSpecs(rootNode, entries.services);
-                loadFolders(entries.folders, entries.services, rootNode);
-            });
-        }
-
-        function addParentNodeToServiceSpecs(parentNode, serviceSpecs) {
-            // When a service is loaded we'll want to know where to hang its tree nodes
-            _.each(serviceSpecs, function (serviceSpec) {
-                serviceSpec.parentNode = parentNode;
-            });
-        }
-
-        function loadFolders(folderNames, serviceSpecs, parentNode) {
-            // Start loading all folders, keeping "deferred" objects so we know when they're done
-            var deferreds = _.map(folderNames, function (folderName) {
-                return loadFolder(folderName, function (entries) {
-                    console.log("Folder " + folderName + " has " + entries.services.length + " services");
-                    // Folder has loaded -- make its node and add its services to "serviceSpecs"
-                    var node = makeContainerNode(folderName, "pluginLayerSelector-folder", parentNode);
-                    addParentNodeToServiceSpecs(node, entries.services);
-                    $.merge(serviceSpecs, entries.services);
-                });
-            });
-            // When all folders have loaded, load the services
-            $.when.apply($, deferreds).then(function () {
-                loadServices(serviceSpecs);
-            });
-        }
-
-        function loadFolder(folderName, success) {
-            console.log("Loading folder '" + folderName + "'");
-            return $.ajax({
-                dataType: 'jsonp',
-                url: baseUrl + (folderName === "" ? "" : "/" + folderName) + '?f=json',
-                success: success,
-                error: handleAjaxError
-            });
-        }
-
-        function loadServices(serviceSpecs) {
-            // Start loading all services, keeping "deferred" objects so we know when they're done
-            console.log("Loading " + serviceSpecs.length + " services");
-            var deferreds = _.map(serviceSpecs, loadService);
-            // When all services have loaded, we're done!
-            $.when.apply($, deferreds).then(function () {
-                console.log("All done!");
-                renderTree();
-            });
-        }
-
-        function loadService(serviceSpec) {
-            if (serviceSpec.type === "MapServer") {
-                console.log("Loading service " + serviceSpec.name);
-                var serviceUrl = baseUrl + "/" + serviceSpec.name + "/MapServer";
-                return $.ajax({
-                    dataType: 'jsonp',
-                    url: serviceUrl + "?f=json",
-                    success: function (serviceData) {
-                        // Service has loaded -- make its node and load its layers
-                        console.log("Service " + serviceSpec.name + " has " + serviceData.layers.length + " layers");
-                        var serviceName = getServiceName(serviceSpec.name);
-                        var node = makeContainerNode(serviceName, "pluginLayerSelector-service", serviceSpec.parentNode);
-                        loadLayers(serviceData.layers, node, serviceUrl);
-                    },
-                    error: handleAjaxError
-                });
-            }
-        }
-
-        function getServiceName(name) {
-            // "Alabama/Bathymetry" => "Bathymetry"
-            return (name.indexOf('/') == -1 ? name : name.split('/')[1]);
-        }                
-
-        function loadLayers(layerSpecs, serviceNode, serviceUrl) {
-            var layerNodes = {};
-            _.each(layerSpecs, function (layerSpec) {
-                // A layer might specify a parent layer; otherwise it hangs off the service
-                var parentNode = (layerSpec.parentLayerId === -1 ? serviceNode : layerNodes[layerSpec.parentLayerId]);
-                if (layerSpec.subLayerIds === null) {
-                    // This is an actual layer
-                    makeLeafNode(layerSpec, serviceUrl, parentNode);
-                } else {
-                    // This is a layer group; remember its node so its children can attach themselves
-                    layerNodes[layerSpec.id] = makeContainerNode(layerSpec.name, "pluginLayerSelector-layer-group", parentNode);
-                }
-            }, this);
-        }
-
-        function makeContainerNode(name, className, parentNode) {
-            var node = {
-                cls: className, // When the tree is displayed the node's associated DOM element will have this CSS class
-                text: name.replace(/_/g, " "),
-                leaf: false,
-                children: []
-            };
-            parentNode.children.push(node);
-            return node;
-        }
-
-        function makeLeafNode(layerSpec, serviceUrl, parentNode) {
-            var node = {
-                cls: "pluginLayerSelector-layer", // When the tree is displayed the node's associated DOM element will have this CSS class
-                text: layerSpec.name.replace(/_/g, " "),
-                leaf: true,
-                checked: false,
-                url: serviceUrl,
-                layerId: layerSpec.id,
-            };
-            parentNode.children.push(node);
-            return node;
-        }
-
-        function handleAjaxError(jqXHR, textStatus, errorThrown) {
-            // TODO: do something better
-            alert('AJAX error: ' + errorThrown);
-        }
-
-        function renderTree() {
-            sortFolders([rootNode]);
-            var store = Ext.create('Ext.data.TreeStore', {
-                root: rootNode,
-                fields: ['text', 'leaf', 'cls', 'url', 'layerId']
-            });
-            var tree = Ext.create('Ext.tree.Panel', {
-                store: store,
-                rootVisible: false,
-                height: 2000,
-                autoScroll: true,
-                title: 'Map Layers',
-                x: 100,
-                y: 100,
-                renderTo: 'pane1',
-                width: 300,
-                height: 240,
-                resizable: true,
-                collapsible: true,
-                autoScroll: true,
-            });
-            tree.show();
-        }
-
-        function sortFolders(nodes) {
-            // Sort folder entries at this level
-            nodes.sort(function (node1, node2) {
-                return node1.text.toLowerCase() > node2.text.toLowerCase() ? 1 : -1;
-            });
-            // Recurse to sort subfolders
-            _.each(nodes, function (node) {
-                if (!node.leaf && node.children.length > 0) {
-                    sortFolders(node.children);
-                }
-            });
-        }
+    ["dojo/_base/declare", "./agsLoader", "./ui"], 
+    function (declare, agsLoader, ui) {
 
         return declare(null, {
             toolbarName: "Map Layers",
             fullName: "Configure and control layers to be overlayed on the base map.",
 
+            _layerTree: null,
+
             constructor: function (args) {
                 declare.safeMixin(this, args);
-                loadCatalog();
+
+                var baseUrl = 'http://dev.gulfmex.coastalresilience.org/arcgis/rest/services';
+                this._layerTree = { expanded: true, children: [] };
+                agsLoader.load(baseUrl, this._layerTree);
+            },
+
+            activate: function () {
+                ui.render(this._layerTree);
             }
+
         });
     }
 );
