d68bba70abc5da5ac792a446ce687979423968c8
Rick Mohr <rmohr@azavea.com>
2013-02-13 14:55:46 -0500

    More work on layer_selector modularization
    AgsLoader is now a class, so we can instantiate more than one. (But due to a Heisenbug we can't currently do that.)
    
    Load Ext JS locally. (See next checkin for actual sources.)
    
    Render tree in activate() method.

--- GeositeFramework/plugins/layer_selector/main.js
+++ GeositeFramework/plugins/layer_selector/main.js
@@ -1,23 +1,30 @@
 ﻿define(
-    ["dojo/_base/declare", "./agsLoader", "./ui"], 
-    function (declare, agsLoader, ui) {
+    ["dojo/_base/declare", "./AgsLoader", "./ui"], 
+    function (declare, AgsLoader, ui) {
 
         return declare(null, {
             toolbarName: "Map Layers",
             fullName: "Configure and control layers to be overlayed on the base map.",
 
             _layerTree: null,
+            _agsLoader: null,
 
             constructor: function (args) {
                 declare.safeMixin(this, args);
 
-                var baseUrl = 'http://dev.gulfmex.coastalresilience.org/arcgis/rest/services';
                 this._layerTree = { expanded: true, children: [] };
-                agsLoader.load(baseUrl, this._layerTree);
+                var baseUrl = 'http://dev.gulfmex.coastalresilience.org/arcgis/rest/services';
+                this._agsLoader = new AgsLoader(baseUrl);
+                this._agsLoader.load(this._layerTree);
             },
 
             activate: function () {
-                ui.render(this._layerTree);
+                if (this._agsLoader.isLoaded()) {
+                    ui.render(this._layerTree);
+                } else {
+                    // TODO: something better
+                    alert("Layers have not finished loading, please try again soon");
+                }
             }
 
         });
