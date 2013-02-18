508ef782250902084cc2751a8465ad0a99b08d69
Rick Mohr <rmohr@azavea.com>
2013-02-18 14:58:21 -0500

    Add BasemapSelector.js to control basemap selector
    Also, keep js imports alphabetized in .html

---
+++ src/GeositeFramework/js/BasemapSelector.js
@@ -0,0 +1,32 @@
+﻿/*jslint nomen:true, devel:true */
+/*global Backbone, _, $, Geosite*/
+
+(function (N) {
+    'use strict';
+    N.models = N.models || {};
+    N.models.BasemapSelector = Backbone.Model.extend({
+        defaults: {
+            basemaps: null,
+            selectedBasemapName: null
+        }
+    });
+}(Geosite));
+
+(function (N) {
+    'use strict';
+    function render(view) {
+        _.each(view.model.get('basemaps'), function (basemap) {
+            var $item = $('<li>').append(basemap.name);
+            view.$('.basemap-selector-list ul').add($item);
+        });
+        return view;
+    }
+
+    N.views = N.views || {};
+    N.views.BasemapSelector = Backbone.View.extend({
+        initialize: function () {
+            return render(this);
+        }
+    });
+
+}(Geosite));
