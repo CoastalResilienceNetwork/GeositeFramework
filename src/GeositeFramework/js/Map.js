e3db9e2ff4e435c186ba526b718fbef16f918a47
Rick Mohr <rmohr@azavea.com>
2013-02-19 14:24:22 -0500

    Map object remembers basemap layers, switches them using show/hide

--- src/GeositeFramework/js/Map.js
+++ src/GeositeFramework/js/Map.js
@@ -4,12 +4,22 @@
 (function (N) {
     'use strict';
 
+    function getSelectedBasemapLayer(model, esriMap) {
+        var basemap = getSelectedBasemap(model);
+        if (basemap.layer === undefined) {
+            // basemap has no layer yet, so make one and cache it
+            basemap.layer = new esri.layers.ArcGISTiledMapServiceLayer(basemap.url);
+            esriMap.addLayer(basemap.layer);
+        }
+        return basemap.layer;
+    }
+
     function getSelectedBasemap(model) {
         var basemaps = model.get('basemaps'),
-            selectedBasemapIndex = model.get('selectedBasemapIndex'),
-            valid = basemaps !== null &&
-                    selectedBasemapIndex !== null &&
-                    selectedBasemapIndex < basemaps.length;
+        selectedBasemapIndex = model.get('selectedBasemapIndex'),
+        valid = basemaps !== null &&
+                selectedBasemapIndex !== null &&
+                selectedBasemapIndex < basemaps.length;
         if (valid) {
             return basemaps[selectedBasemapIndex];
         } else {
@@ -22,10 +32,14 @@
     N.models.Map = Backbone.Model.extend({
         defaults: {
             basemaps: null,
-            selectedBasemapIndex: null
+            selectedBasemapIndex: null   // Both the map view and the basemap selector listen for changes to this attribute
+        },
+        initialize: function () {
+            // deep-copy 'basemaps' because we're going to modify its elements by adding 'layer' properties
+            this.set('basemaps', $.extend(true, [], this.get('basemaps')));
         },
         getSelectedBasemapName: function () { return getSelectedBasemap(this).name; },
-        getSelectedBasemapUrl:  function () { return getSelectedBasemap(this).url; }
+        getSelectedBasemapLayer:  function (esriMap) { return getSelectedBasemapLayer(this, esriMap); }
     });
 }(Geosite));
 
@@ -42,12 +56,13 @@
         var domId = "map" + view.options.paneNumber;
         view.$el.attr("id", domId);
         var esriMap = new esri.Map(domId);
+        N.map = esriMap;
 
         var x = view.model.get('initialExtent');
         esriMap.setExtent(new esri.geometry.Extent(x[0], x[1], x[2], x[3], new esri.SpatialReference({ wkid: 4326 /*lat-long*/ })));
 
         view.esriMap = esriMap;
-        view.model.set('selectedBasemapIndex', 0); // triggers call to selectBasemap()Sybil
+        view.model.set('selectedBasemapIndex', 0); // triggers call to selectBasemap()
 
         function resizeMap() {
             esriMap.resize();
@@ -58,9 +73,12 @@
     }
 
     function selectBasemap(view) {
-        var url = view.model.getSelectedBasemapUrl(),
-            baseMapLayer = new esri.layers.ArcGISTiledMapServiceLayer(url);
-        view.esriMap.addLayer(baseMapLayer);
+        if (view.currentBasemapLayer !== undefined) {
+            view.currentBasemapLayer.hide();
+        }
+        view.currentBasemapLayer = view.model.getSelectedBasemapLayer(view.esriMap);
+        view.currentBasemapLayer.show();
+        view.esriMap.reorderLayer(view.currentBasemapLayer, 0);
     }
 
     N.views = N.views || {};
