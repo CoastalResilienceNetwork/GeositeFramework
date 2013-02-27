50ee4f51d7cc1a84c9aa761062330eca2bfc9cb9
Rick Mohr <rmohr@azavea.com>
2013-02-27 09:39:01 -0500

    Add search box and use it to filter tree

--- src/GeositeFramework/plugins/layer_selector/ui.js
+++ src/GeositeFramework/plugins/layer_selector/ui.js
@@ -1,32 +1,59 @@
 ﻿// Module Ui.js
 
-define(["jquery", "use!underscore", "use!extjs"],
-    function ($, _, Ext) {
+define(["jquery", "use!underscore", "use!extjs", "./treeFilter"],
+    function ($, _, Ext, treeFilter) {
         var Ui = function (container, map) {
             var _map = map,
                 _container = container,
+                _$filterInput = null,
+                _$treeContainer = null,
                 _tree = null;
 
             this.render = function (rootNode) {
                 sortFolders([rootNode]);
-                removeSpinner();
                 _tree = createTree(rootNode);
                 _tree.on("checkchange", onCheckboxChanged, this);
+                removeSpinner();
+                renderUi();
                 this.display();
             }
 
             this.display = function () {
+                // The Ext tree doesn't render right unless the container is visible
                 if (_tree !== null && $(_container).is(":visible")) {
-                    _tree.render(_container);
+                    _tree.render(_$treeContainer[0]);
+                    _$filterInput.focus();
                 }
             }
 
             function addSpinner() {
-                $(_container).append("<div class='pluginLayerSelector-spinner'></div>");
+                $(_container).append(
+                    $('<div>', { 'class': 'pluginLayerSelector-spinner' })
+                );
             }
 
             function removeSpinner() {
                 $(_container).empty();
+            }
+
+            function renderUi() {
+                _$filterInput = $('<input>', {
+                    type: 'text',
+                    placeholder: 'Search layers',
+                    keyup: onFilterBoxKeyup
+                });
+                _$treeContainer = $('<div>', {
+                    'class': 'pluginLayerSelector-tree-container'
+                });
+                $(_container).append(
+                    $('<div>', { 'class': 'pluginLayerSelector-search' }).append(
+                        _$filterInput,
+                        $('<button>').hide()
+                    ),
+                    $('<div>', { 'class': 'pluginLayerSelector-rest' }).append(
+                        _$treeContainer
+                    )
+                );
             }
 
             function sortFolders(nodes) {
@@ -42,12 +69,17 @@ define(["jquery", "use!underscore", "use!extjs"],
                 });
             }
 
+            Ext.define('FilteredTreePanel', {
+                extend: 'Ext.tree.Panel',
+                mixins: { treeFilter: 'layer_selector.lib.TreeFilter' }
+            });
+
             function createTree(rootNode) {
                 var store = Ext.create('Ext.data.TreeStore', {
                     root: rootNode,
                     fields: ['text', 'leaf', 'cls', 'url', 'layerId']
                 });
-                var tree = Ext.create('Ext.tree.Panel', {
+                var tree = Ext.create('FilteredTreePanel', {
                     store: store,
                     rootVisible: false,
                     width: '200%',
@@ -59,6 +91,15 @@ define(["jquery", "use!underscore", "use!extjs"],
             function onCheckboxChanged(node, checked, eOpts) {
                 var layerData = node.raw;
                 layerData.showOrHideLayer(layerData, checked, _map);
+            }
+
+            function onFilterBoxKeyup(event) {
+                var text = _$filterInput.val();
+                if (text === "") {
+                    _tree.clearFilter();
+                } else {
+                    _tree.filterByText(text);
+                }
             }
 
             addSpinner();
