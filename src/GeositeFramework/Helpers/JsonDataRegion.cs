4dcee46adbab3d3750a8dccf83ddac506753ca45
Rick Mohr <rmohr@azavea.com>
2013-03-18 13:54:31 -0400

    Add JsonData.cs to load and validate JSON data
    Subclasses JsonDataRegion and JsonDataPlugin handle region.json file and plugin.json files, respectively, by specifying a JSON Schema file.
    
    A key benefit of abstracting this way is convenient support for both app code and test code. JSON data is validated in one place whether it's from a file (read by app setup code) or a string (defined by test setup code). If errors happen the message can include a specific JSON file path because that path is preserved on the JsonData object.

---
+++ src/GeositeFramework/Helpers/JsonDataRegion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace GeositeFramework.Helpers
+{
+
+    /// <summary>
+    /// Represents JSON configuration data for a Geosite (e.g. from a "region.json" file)
+    /// </summary>
+    public class JsonDataRegion : JsonData
+    {
+        public JsonDataRegion(string schemaFolderPath)
+            : base(Path.Combine(schemaFolderPath, "regionSchema.json")) { }
+    }
+
+}
