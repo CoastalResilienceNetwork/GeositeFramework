7f1035d19ecfd55ffd167940b05b16eb2c334a22
Rick Mohr <rmohr@azavea.com>
2013-03-18 14:13:15 -0400

    Add tests for plugin.config files
    ... and refactor to be DRYer

--- src/GeositeFramework/Tests/GeositeTests.cs
+++ src/GeositeFramework/Tests/GeositeTests.cs
@@ -6,18 +6,35 @@ using System.Web;
 using GeositeFramework.Models;
 using Newtonsoft.Json.Linq;
 using System.IO;
+using GeositeFramework.Helpers;
 
 namespace GeositeFramework.Tests
 {
     [TestFixture]
     public class GeositeTests : AssertionHelper
     {
-        private static readonly List<string> dummyPluginFolderNames = new List<string>();
-        private static readonly string appDataFolderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\App_Data");
+        private static readonly string _appDataFolderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\App_Data");
 
-        /// <exclude/>
+        // ------------------------------------------------------------------------
+        // Tests for region.json configuration
+
+        private Geosite CreateGeosite(string regionJson)
+        {
+            return new Geosite(LoadRegionData(regionJson), new List<string>(), null);
+        }
+
+        private Geosite CreateGeosite(string regionJson, List<string> pluginFolderNames)
+        {
+            return new Geosite(LoadRegionData(regionJson), pluginFolderNames, null);
+        }
+
+        private JsonData LoadRegionData(string regionJson)
+        {
+            return new JsonDataRegion(_appDataFolderPath).LoadText(regionJson);
+        }
+
         [Test]
-        public void TestSuccess()
+        public void TestValidRegionData()
         {
             var regionJson = @"
                 {
@@ -37,7 +54,8 @@ namespace GeositeFramework.Tests
                     'pluginOrder': [ 'layer_selector', 'measure' ]
                 }";
             var pluginFolderNames = new List<string> { "nearshore_waves", "measure", "layer_selector", "explode"};
-            var geosite = new Geosite(regionJson, pluginFolderNames, appDataFolderPath);
+            var pluginJsonData = new List<JsonData> { LoadPluginData(@"{ css: ['main.css'], use: { underscore: { attach: '_' } } }") };
+            var geosite = new Geosite(LoadRegionData(regionJson), pluginFolderNames, pluginJsonData);
 
             Expect(geosite.Organization, EqualTo("Sample Org"));
             Expect(geosite.Title, EqualTo("Geosite Framework Sample"));
@@ -46,6 +64,8 @@ namespace GeositeFramework.Tests
             Expect(geosite.HeaderLinks[1].Text, EqualTo("GIS"));
             Expect(geosite.PluginModuleIdentifiers, EqualTo("'plugins/layer_selector/main', 'plugins/measure/main', 'plugins/nearshore_waves/main', 'plugins/explode/main'"));
             Expect(geosite.PluginVariableNames, EqualTo("p0, p1, p2, p3"));
+            Expect(geosite.PluginCssUrls, Contains("main.css"));
+            Expect(geosite.ConfigurationForUseJs, Contains("underscore"));
 
             // Test that "pluginFolderNames" was correctly added to the JSON
             var jsonObj = JObject.Parse(geosite.RegionDataJson);
@@ -53,30 +73,27 @@ namespace GeositeFramework.Tests
             Expect(orderedFolderNames, EqualTo(new List<string> { "layer_selector", "measure", "nearshore_waves", "explode" }));
         }
 
-        /// <exclude/>
         [Test]
-        [ExpectedException(typeof(GeositeJsonParseException), Handler = "HandleInvalidJson")]
-        public void TestInvalidJson()
+        [ExpectedException(typeof(JsonValidationException), Handler = "HandleInvalidRegionJson")]
+        public void TestInvalidRegionJson()
         {
-            var regionJson = @"This is not valid JSON {['";
-            new Geosite(regionJson, dummyPluginFolderNames, appDataFolderPath);
+            CreateGeosite(@"This is not valid JSON {['");
         }
-        private void HandleInvalidJson(Exception ex)
+        private void HandleInvalidRegionJson(Exception ex)
         {
-            Expect((ex as GeositeJsonParseException).ParseMessages[0], Contains("Unexpected character"));
+            Expect((ex as JsonValidationException).ParseMessages[0], Contains("Unexpected character"));
         }
 
         /// <exclude/>
         [Test]
-        [ExpectedException(typeof(GeositeJsonParseException), Handler = "HandleWrongProperties")]
-        public void TestWrongProperties()
+        [ExpectedException(typeof(JsonValidationException), Handler = "HandleWrongRegionJsonProperties")]
+        public void TestWrongRegionJsonProperties()
         {
-            var regionJson = @"{'a':'', 'b':''}";
-            new Geosite(regionJson, dummyPluginFolderNames, appDataFolderPath);
+            CreateGeosite(@"{'a':'', 'b':''}");
         }
-        private void HandleWrongProperties(Exception ex)
+        private void HandleWrongRegionJsonProperties(Exception ex)
         {
-            var messages = (ex as GeositeJsonParseException).ParseMessages;
+            var messages = (ex as JsonValidationException).ParseMessages;
             Expect(messages.Count, EqualTo(3));
             Expect(messages[0], Contains("'a'")); // extra property
             Expect(messages[1], Contains("'b'")); // extra property
@@ -85,8 +102,8 @@ namespace GeositeFramework.Tests
 
         /// <exclude/>
         [Test]
-        [ExpectedException(typeof(GeositeJsonParseException), Handler = "HandleBadExtent")]
-        public void TestBadExtent()
+        [ExpectedException(typeof(JsonValidationException), Handler = "HandleBadRegionJsonExtent")]
+        public void TestBadRegionJsonExtent()
         {
             var regionJson = @"
                 {
@@ -95,11 +112,11 @@ namespace GeositeFramework.Tests
                     'initialExtent': [0,0,0],
                     'basemaps': [{'name':'', 'url':''}],
                 }";
-            new Geosite(regionJson, dummyPluginFolderNames, appDataFolderPath);
+            CreateGeosite(regionJson);
         }
-        private void HandleBadExtent(Exception ex)
+        private void HandleBadRegionJsonExtent(Exception ex)
         {
-            Expect((ex as GeositeJsonParseException).ParseMessages[0], Contains("3 is less than minimum count of 4"));
+            Expect((ex as JsonValidationException).ParseMessages[0], Contains("3 is less than minimum count of 4"));
         }
 
         /// <exclude/>
@@ -116,7 +133,113 @@ namespace GeositeFramework.Tests
                     'pluginOrder': [ 'layer_selector', 'XYZ' ]
                 }";
             var pluginFolderNames = new List<string> { "nearshore_waves", "measure", "layer_selector", "explode" };
-            new Geosite(regionJson, pluginFolderNames, appDataFolderPath);
+            CreateGeosite(regionJson, pluginFolderNames);
         }
+
+        // ------------------------------------------------------------------------
+        // Tests for plugin.json configuration
+
+        private Geosite CreateGeosite(List<JsonData> pluginConfigJsonData)
+        {
+            string regionJson = @"
+                {
+                    'organization': '',
+                    'title': '',
+                    'initialExtent': [0,0,0,0],
+                    'basemaps': [{'name':'', 'url':''}]
+                }";
+            return new Geosite(LoadRegionData(regionJson), new List<string>(), pluginConfigJsonData);
+        }
+
+        private JObject ParsePluginData(string pluginJson)
+        {
+            return LoadPluginData(pluginJson).Validate();
+        }
+
+        private JsonData LoadPluginData(string pluginJson)
+        {
+            return new JsonDataPlugin(_appDataFolderPath).LoadText(pluginJson);
+        }
+
+        [Test]
+        public void TestValidPluginData()
+        {
+            var pluginJson = @"
+                {
+                    css: [
+                        'plugins/layer_selector/main.css',
+                        '//cdn.sencha.io/ext-4.1.1-gpl/resources/css/ext-all.css'
+                    ],
+                    use: {
+                        underscore: { attach: '_' },
+                        tv4: { attach: 'tv4' },
+                        extjs: { attach: 'Ext' }
+                    }
+                }";
+            var jsonObj = ParsePluginData(pluginJson);
+
+            Expect(jsonObj.GetValue("css").Values<string>().Count(), EqualTo(2));
+            Expect(jsonObj.GetValue("use").Values<JProperty>().Count(), EqualTo(3));
+        }
+
+        [Test]
+        [ExpectedException(typeof(JsonValidationException), Handler = "HandleInvalidPluginJson")]
+        public void TestInvalidPluginJson()
+        {
+            ParsePluginData(@"This is not valid JSON {['");
+        }
+        private void HandleInvalidPluginJson(Exception ex)
+        {
+            Expect((ex as JsonValidationException).ParseMessages[0], Contains("Unexpected character"));
+        }
+
+        [Test]
+        [ExpectedException(typeof(JsonValidationException), Handler = "HandleWrongPluginJsonProperties")]
+        public void TestWrongPluginJsonProperties()
+        {
+            ParsePluginData(@"{a:'', b:''}");
+        }
+        private void HandleWrongPluginJsonProperties(Exception ex)
+        {
+            var messages = (ex as JsonValidationException).ParseMessages;
+            Expect(messages.Count, EqualTo(2));
+            Expect(messages[0], Contains("'a'")); // extra property
+            Expect(messages[1], Contains("'b'")); // extra property
+        }
+
+        [Test]
+        [ExpectedException(typeof(JsonValidationException), Handler = "HandlePluginCssNotList")]
+        public void TestPluginCssNotList()
+        {
+            ParsePluginData(@"{css: 'foo.css'}");
+        }
+        private void HandlePluginCssNotList(Exception ex)
+        {
+            Expect((ex as JsonValidationException).ParseMessages[0], Contains("Expected Array but got String"));
+        }
+
+        [Test]
+        [ExpectedException(typeof(JsonValidationException), Handler = "HandlePluginUseNotObject")]
+        public void TestPluginUseNotObject()
+        {
+            ParsePluginData(@"{use: 'underscore'}");
+        }
+        private void HandlePluginUseNotObject(Exception ex)
+        {
+            Expect((ex as JsonValidationException).ParseMessages[0], Contains("Expected Object but got String"));
+        }
+
+        [Test]
+        [ExpectedException(typeof(ApplicationException), ExpectedMessage = "clause 'underscore' differently", MatchType = MessageMatch.Contains)]
+        public void TestPluginUseConflict()
+        {
+            var jsonData = new List<JsonData> 
+            {
+                LoadPluginData(@"{ use: { underscore: { attach: '_' } } }"),
+                LoadPluginData(@"{ use: { underscore: { attach: '_2' } } }")
+            };
+            CreateGeosite(jsonData);
+        }
+
     }
 }
