9b2e069b4b709495ba1bf519090eb57ad6762099
Rick Mohr <rmohr@azavea.com>
2014-01-21 12:31:12 -0500

    Focus layers.json for testing
    * Simplify layers.json
    * Add error cases
    * Allow comments in JSON (strip before parsing)
    * Add comments to layers.json

--- src/GeositeFramework/plugins/layer_selector/LayerManager.js
+++ src/GeositeFramework/plugins/layer_selector/LayerManager.js
@@ -4,11 +4,12 @@ define([
         "dojo/json",
         "use!tv4",
         "use!underscore",
+        "./stripJsonComments",
         "./layerConfigSchema",
         "./AgsLoader",
         "./WmsLoader"
     ],
-    function (JSON, tv4, _, layerConfigSchema, AgsLoader, WmsLoader) {
+    function (JSON, tv4, _, stripJsonComments, layerConfigSchema, AgsLoader, WmsLoader) {
 
         var LayerManager = function (app) {
             var _app = app,
@@ -109,7 +110,8 @@ define([
                 // Parse and validate config data to get URLs of layer sources
                 var errorMessage;
                 try {
-                    var data = JSON.parse(layerSourcesJson),
+                    var json = stripJsonComments(layerSourcesJson),
+                        data = JSON.parse(json),
                         valid = tv4.validate(data, layerConfigSchema);
                     if (valid) {
                         return data;
---
+++ src/GeositeFramework/plugins/layer_selector/stripJsonComments.js
@@ -0,0 +1,61 @@
+﻿/*!
+        strip-json-comments
+        Strip comments from JSON. Lets you use comments in your JSON files!
+        https://github.com/sindresorhus/strip-json-comments
+        by Sindre Sorhus
+        MIT License
+        Version 0.1.1
+*/
+define(function () {
+    'use strict';
+
+    function stripJsonComments(str) {
+        var currentChar;
+        var nextChar;
+        var insideString = false;
+        var insideComment = false;
+        var ret = '';
+
+        for (var i = 0; i < str.length; i++) {
+            currentChar = str[i];
+            nextChar = str[i + 1];
+
+            if (!insideComment && str[i - 1] !== '\\' && currentChar === '"') {
+                insideString = !insideString;
+            }
+
+            if (insideString) {
+                ret += currentChar;
+                continue;
+            }
+
+            if (!insideComment && currentChar + nextChar === '//') {
+                insideComment = 'single';
+                i++;
+            } else if (insideComment === 'single' && currentChar + nextChar === '\r\n') {
+                insideComment = false;
+                i++;
+            } else if (insideComment === 'single' && currentChar === '\n') {
+                insideComment = false;
+            } else if (!insideComment && currentChar + nextChar === '/*') {
+                insideComment = 'multi';
+                i++;
+                continue;
+            } else if (insideComment === 'multi' && currentChar + nextChar === '*/') {
+                insideComment = false;
+                i++;
+                continue;
+            }
+
+            if (insideComment) {
+                continue;
+            }
+
+            ret += currentChar;
+        }
+
+        return ret;
+    }
+
+    return stripJsonComments;
+});
