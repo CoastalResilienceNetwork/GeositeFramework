3be9e904513abcb8c038431c63fed53ed4a89924
Steve Lamb <slamb@azavea.com>
2014-02-13 17:55:29 -0500

    Allow plugins 5 seconds to identify before timeout
    5 seconds was chosen arbitrarily, to prevent indefinite requests. I found 350 milliseconds to be the sweet spot where some Layer Selector requests fail and some succeed, at least for AGS servers. This variable can be configured over time.

--- src/GeositeFramework/js/Identify.js
+++ src/GeositeFramework/js/Identify.js
@@ -7,11 +7,22 @@
 // about that point.
 
 (function (N) {
+
+    dojo.require("esri.dijit.Popup");
+
+    var IDENTIFY_TIMEOUT = 5000,
+        TIMEOUT_MESSAGE = 'Failed to return results within ' + IDENTIFY_TIMEOUT / 1000 + ' seconds',
+        DEFAULT_WINDOW_WIDTH = 300,
+        DEFAULT_WINDOW_HEIGHT = 600;
+
+
     N.doIdentify = function (view, pluginModels, event) {
         var map = view.esriMap,
-            windowWidth = 300,
-            windowHeight = 600,
-            infoWindow = createIdentifyWindow(view, map, event, windowWidth, windowHeight),
+            windowWidth = DEFAULT_WINDOW_WIDTH,
+            windowHeight = DEFAULT_WINDOW_HEIGHT,
+            infoWindow = createIdentifyWindow(view, map, event),
+            // create an element for plugins to write their results into, or, if a timeout
+            // occurs, for the framework to write a timeout message into.
             $resultsContainer = $('<div>').addClass('identify-results'),
             showIfLast = _.after(pluginModels.length, function () {
                 showIdentifyResults(infoWindow, $resultsContainer, windowWidth, windowHeight);
@@ -19,33 +30,45 @@
 
         // Accumulate results (probably asynchronously), and show them when all are accumulated
         pluginModels.each(function (pluginModel) {
-            var clickPoint = { x: event.x, y: event.y };
-            pluginModel.identify(event.mapPoint, clickPoint, processResults);
+            var clickPoint = { x: event.x, y: event.y },
+                renderTimeout = function (args) {
+                    var timeoutNotice = $('<div>').text(TIMEOUT_MESSAGE),
+                        toolbarName = pluginModel.get('pluginObject').toolbarName;
+
+                    appendIdentifyResult(toolbarName, timeoutNotice, $resultsContainer);
+                },
+                processResults = function processResults(args) {
+                    if (args.result) {
+                        appendIdentifyResult(args.pluginTitle, args.result, $resultsContainer);
+                        if (args.width) { windowWidth = Math.max(windowWidth, args.width); }
+                        if (args.height) { windowHeight = Math.max(windowHeight, args.height); }
+                    }
+                },
+                processOrTimeout = N.timeoutWrapper({ success: processResults,
+                                                      failure: renderTimeout,
+                                                      executeOnce: showIfLast,
+                                                      ms: IDENTIFY_TIMEOUT });
+
+            pluginModel.identify(event.mapPoint, clickPoint, processOrTimeout);
         });
 
-        function processResults(pluginTitle, result, width, height) {
-            if (result) {
-                var template = N.app.templates['template-result-of-identify'],
-                    $html = $($.trim(template({ pluginTitle: pluginTitle })));
-                $html.find('.identify-result').append(result);
-                $resultsContainer.append($html);
-                if (width) { windowWidth = Math.max(windowWidth, width); }
-                if (height) { windowHeight = Math.max(windowHeight, height); }
-            }
-            showIfLast();
-        }
     };
 
-    dojo.require("esri.dijit.Popup");
+    function appendIdentifyResult(pluginTitle, resultEl, $resultsContainer) {
+        var template = N.app.templates['template-result-of-identify'],
+            $html = $($.trim(template({ pluginTitle: pluginTitle })));
+        $html.find('.identify-result').append(resultEl);
+        $resultsContainer.append($html);
+    }
 
-    function createIdentifyWindow(view, map, event, width, height) {
-        map.infoWindow.destroy();
-
+    function createIdentifyWindow(view, map, event) {
         // Create a new info window
         var $infoWindow = $('<div>').addClass('identify-info-window').appendTo(view.$infoWindowParent),
             infoWindow = new esri.dijit.Popup({ map: map }, $infoWindow.get(0));
+
+        map.infoWindow.destroy();
         map.infoWindow = infoWindow;
-        infoWindow.resize(width, height);
+        infoWindow.resize(DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT);
         infoWindow.setTitle(""); // without this call the title bar is hidden, along with its controls
         $infoWindow.find('.spinner').removeClass('hidden');
         infoWindow.show(event.mapPoint);
