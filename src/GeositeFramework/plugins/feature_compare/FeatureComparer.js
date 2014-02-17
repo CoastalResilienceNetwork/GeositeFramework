456d1b67037254254a48a1d448fb10b93e5353bf
Matthew McFarland <mmcfarland@azavea.com>
2014-02-17 10:49:08 -0500

    Add schema validation for FeatureCompare

--- src/GeositeFramework/plugins/feature_compare/FeatureComparer.js
+++ src/GeositeFramework/plugins/feature_compare/FeatureComparer.js
@@ -1,11 +1,14 @@
-﻿define(["use!underscore", "./Comparer"],
+﻿define(["dojo/json", "use!tv4", "dojo/text!./comparerSchema.json", "use!underscore", "./Comparer"],
 
-    function(_, compare) {
+    function(JSON, tv4, comparerSchema, _, compare) {
 
         var FeatureComparer = function(options) {
-            
+            this.app = options.context.app;
+            this.config = this._parseConfig(tv4, options.config, comparerSchema);
+            if (!this.config) { return; }
+
             this.$templates = $('<div>').append($($.trim(options.templates)));
-            this.comparer = new compare.Comparer(options.config, {
+            this.comparer = new compare.Comparer(this.config, {
                 map: options.context.map
             });
 
@@ -34,17 +37,35 @@
             this.comparer.hide();
         };
 
-        FeatureComparer.prototype._getTemplate = function(name) {
-            var template = _.template($.trim(this.$templates.find('#' + name).html()));
-            return template;
-        };
-
         FeatureComparer.prototype.getState = function() {
             return this.comparer.getState();
         };
 
         FeatureComparer.prototype.setInitialState = function(state) {
             this.comparer.setInitialState(state);
+        };
+
+        FeatureComparer.prototype._getTemplate = function(name) {
+            var template = _.template($.trim(this.$templates.find('#' + name).html()));
+            return template;
+        };
+
+        FeatureComparer.prototype._parseConfig = function(validator, config, schema) {
+            /* Validate the user configured options against the plugin schema */
+            var errorMessage;
+            try {
+                if (validator.validate(config, schema)) {
+                    return config;
+                }
+                errorMessage = tv4.error.message + " (data path:in " + tv4.error.dataPath + ")";
+
+            } catch(ex) {
+                errorMessage = ex.message;
+            }
+            this.app.error("", "Error in Feature Compare plugin config: " +
+                errorMessage);
+
+            return null;
         };
 
         return FeatureComparer;
