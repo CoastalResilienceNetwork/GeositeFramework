580f5a6201219980c1defa80d40c84c59a822791
Kevin DeLoach <kdeloach@azavea.com>
2014-03-04 12:21:32 -0500

    Add sample plugin (Custom Map Layers) to demonstrate ability to inherit from other plugins.

---
+++ src/GeositeFramework/sample_plugins/custom_layer_selector/main.js
@@ -0,0 +1,18 @@
+﻿
+define([
+    "dojo/_base/declare",
+    "plugins/layer_selector/main",
+    "dojo/text!./layers.json"],
+    function (declare,
+              LayerSelectorPlugin,
+              layerSourcesJson) {
+        return declare(LayerSelectorPlugin, {
+            toolbarName: "Custom Map Layers",
+            fullName: "Map layers plugin with custom layers.json",
+
+            getLayersJson: function() {
+                return layerSourcesJson;
+            }
+        });
+    }
+);
