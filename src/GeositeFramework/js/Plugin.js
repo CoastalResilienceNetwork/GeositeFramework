62119088f69e698fe0ac7b532cd29aff7e7df51b
Matthew McFarland <mmcfarland@azavea.com>
2015-10-21 17:37:53 -0400

    Pass reference to print button to plugin
    If a plugin is using the custom plugin print, it may want to control the
    times at which the print button is available to the user (for example,
    after the last step of a linear sequence when the data is availabel).

--- src/GeositeFramework/js/Plugin.js
+++ src/GeositeFramework/js/Plugin.js
@@ -33,7 +33,7 @@ require(['use!Geosite',
                 $uiContainer = model.get('$uiContainer'),
                 $legendContainer = model.get('$legendContainer'),
                 logger = new Logger(pluginName);
-            
+
             pluginObject.initialize({
                 app: {
                     version: N.app.version,
@@ -48,8 +48,9 @@ require(['use!Geosite',
                 },
                 map: N.createMapWrapper(esriMap, mapModel, pluginObject),
                 container: ($uiContainer ? $uiContainer.find('.plugin-container-inner')[0] : undefined),
-                legendContainer: ($legendContainer ? $legendContainer[0] : undefined)
-            });
+                legendContainer: ($legendContainer ? $legendContainer[0] : undefined),
+                printButton: ($uiContainer ? $uiContainer.find('.plugin-print'): undefined )
+        });
         }
 
         function requestCsvDownload(filename, content) {
