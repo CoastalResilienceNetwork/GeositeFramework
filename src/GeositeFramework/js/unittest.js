e6f957e30d4a86c9939e44b0415b2b105c008ab7
Kevin DeLoach <kdeloach@gmail.com>
2016-02-15 16:36:57 -0500

    Add simple test runner
    This is a very simple test runner for executing unit tests.
    
    Asynchronous unit tests are supported by accepting 1 argument which is a
    "done" callback. Tests that take longer than 1000 ms will timeout and fail.
    
    Run all unit tests by requiring tests.js from the plugin main.js file.

---
+++ src/GeositeFramework/js/unittest.js
@@ -0,0 +1,54 @@
+define([
+        "dojo/Deferred",
+        "dojo/promise/all",
+        "underscore"
+    ],
+    function(Deferred,
+             all,
+             _) {
+        "use strict";
+
+        function runAllTests(tests) {
+            var promise = all(_.map(tests, function(test) {
+                var defer = new Deferred(),
+                    done = function() {
+                        defer.resolve();
+                    };
+
+                // Fail the test if it takes longer than 1000 ms to execute.
+                setTimeout(function() {
+                    if (!defer.isFulfilled()) {
+                        console.debug(test.name + ' did not complete after the timeout period');
+                        defer.reject();
+                    }
+                }, 1000);
+
+                // Pass a "done" callback to any test that accepts 1 argument.
+                if (test.length === 1) {
+                    test(done);
+                } else {
+                    test();
+                    done();
+                }
+
+                return defer;
+            }));
+            promise.then(function() {
+                console.debug('All tests passed!');
+            }, function() {
+                console.debug('Some tests failed.');
+            });
+        }
+
+        function assertTrue(value) {
+            if (!value) {
+                throw new Error('Assertion failed');
+            }
+        }
+
+        return {
+            assertTrue: assertTrue,
+            runAllTests: runAllTests
+        };
+    }
+);
