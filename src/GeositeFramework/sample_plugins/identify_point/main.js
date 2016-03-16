6d710178e24d2a39cd4723477fce3b10c73c2080
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2016-03-16 18:28:12 -0400

    Internationalize sample plugin
    This localizes a sample plugin to demonstrate how others may be localized.

--- src/GeositeFramework/sample_plugins/identify_point/main.js
+++ src/GeositeFramework/sample_plugins/identify_point/main.js
@@ -16,14 +16,14 @@ define(["dojo/_base/declare", "framework/PluginBase"],
             initialize: function(args) {
                 declare.safeMixin(this, args);
                 $(this.container).append(
-                    '<h4 style="padding: 5px;">Click any point on the map to display Latitude and Longitude</h4>');
+                    '<h4 style="padding: 5px;">' + i18next.t('Click any point on the map to display Latitude and Longitude') + '</h4>');
 
                 // Hide the print button until the identify feature has been used.
                 $(this.printButton).hide();
             },
 
             identify: function(mapPoint, clickPoint, processResults) {
-                var text = "You clicked on latitude " + mapPoint.getLatitude() + " longitude " + mapPoint.getLongitude(),
+                var text = i18next.t("You clicked on latitude %f longitude %f", mapPoint.getLatitude(), mapPoint.getLongitude()),
                     identifyWidth = 300;
                 processResults(text, identifyWidth);
 
@@ -36,7 +36,7 @@ define(["dojo/_base/declare", "framework/PluginBase"],
                 var layer = new esri.layers.ArcGISDynamicMapServiceLayer("http://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Demographics/ESRI_Population_World/MapServer", {
                         "opacity": 0.8
                     });
-                
+
                 mapObject.addLayer(layer);
 
                 $printArea.append('<img id="sample-graphic-print" src="' + this.infoGraphic + '" >');
