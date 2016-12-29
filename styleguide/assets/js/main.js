6db29aa85c35e862aab8c1f2e3ceda8966812c24
Matthew McFarland <mmcfarland@azavea.com>
2016-12-29 10:51:48 -0500

    Style guide source and static assets
    The styleguide is a mock framework index page with a mock plugin
    demonstrating various markup and style components.  Its source is sass but
    it has compiled css assets

---
+++ styleguide/assets/js/main.js
@@ -0,0 +1,18 @@
+var map = L.map('map', {
+  center: [40.000, -75.1639],
+  zoom: 12
+});
+var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  subdomains: 'abcd',
+  minZoom: 0,
+  maxZoom: 20,
+  ext: 'png'
+}).addTo(map);
+
+
+// Map Attribution
+// Register click event
+$('.map-attribution').click(function(e){
+  $(this).toggleClass('active');
+})
