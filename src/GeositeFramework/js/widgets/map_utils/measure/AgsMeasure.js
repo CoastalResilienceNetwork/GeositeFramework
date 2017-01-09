e7f91cea5f78a4a47fbb0b9a88f44175dac552e5
Kelly Innes <kinnes@azavea.com>
2017-01-09 13:56:36 -0500

    Import webMercatorUtils from /geometry/support

--- src/GeositeFramework/js/widgets/map_utils/measure/AgsMeasure.js
+++ src/GeositeFramework/js/widgets/map_utils/measure/AgsMeasure.js
@@ -4,7 +4,7 @@ define([
         "esri/geometry/Polyline",
         "esri/geometry/Polygon",
         "esri/geometry/geodesicUtils",
-        "esri/geometry/webMercatorUtils",
+        "esri/geometry/support/webMercatorUtils",
         "esri/tasks/GeometryService",
         "esri/geometry/ScreenPoint",
         "esri/geometry/mathUtils",
