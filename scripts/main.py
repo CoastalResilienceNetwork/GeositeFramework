4294da8ab71ef4214cca5893459f65573daca3ac
Jenny Fung <jfung@azavea.com>
2018-09-20 09:50:22 -0400

    Add docker flag to server bash script
    For uniform work-flow we want the same scripts to work for those wanting to user the docker environment or their local machine. The only distinction is adding a docker flag (-d) when executing a script from the cli.
    
    Refs #1086

--- scripts/main.py
+++ scripts/main.py
@@ -5,7 +5,7 @@ import json
 from jinja2 import Environment, FileSystemLoader
 
 def convert_region_json():
-    with open('./GeositeFramework/region.json') as f:
+    with open('./src/GeositeFramework/region.json') as f:
         data = json.load(f)
         return data
 
@@ -13,11 +13,11 @@ def template_index():
     j2_env = Environment(loader=FileSystemLoader(''),
                          trim_blocks=True)
     region_json = convert_region_json()
-    templated = j2_env.get_template('./GeositeFramework/template_index.html').render(region_json)
+    templated = j2_env.get_template('./src/GeositeFramework/template_index.html').render(region_json)
 
     # write jinja template to disk, to be used in geosite static assets build
     # as well as served the this project's development server
-    with open("./GeositeFramework/index.html", "wb") as f:
+    with open("./src/GeositeFramework/index.html", "wb") as f:
         f.write(templated)
 
 if __name__ == '__main__':
